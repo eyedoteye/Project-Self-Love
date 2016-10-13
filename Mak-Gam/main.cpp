dd2e5ef52029710eba62c8ee43f6a5f52f94c678
sidecode <sarkar.krishnan@gmail.com>
2016-10-13 11:10:50 -0400

    Add waypoint system

--- Mak-Gam/main.cpp
+++ Mak-Gam/main.cpp
@@ -17,21 +17,18 @@ global_variable bool Running = true;
 global_variable SDL_Point Clicks[255];
 global_variable uint8_t ClicksSize;
 
+global_variable SDL_Point Paths[255];
+global_variable uint8_t PathSize;
+
 SDL_Window *window;
 SDL_Renderer *renderer;
 
-internal void
-RecordMouseClick(int x, int y)
+internal double
+GetDistanceBetweenPoints(double X1, double Y1, double X2, double Y2)
 {
-	Clicks[ClicksSize].x = x;
-	Clicks[ClicksSize].y = y;
-	ClicksSize++;
-}
-
-internal void
-ClearMouseClicks()
-{
-	ClicksSize = 0;
+	double X = X2 - X1;
+	double Y = Y2 - Y1;
+	return sqrt(X * X + Y * Y);
 }
 
 internal double
@@ -40,8 +37,78 @@ GetAngleBetweenPoints(SDL_Point One, SDL_Point Two)
 	int xOff = Two.x - One.x;
 	int yOff = Two.y - One.y;
 
-	return atan2(yOff, xOff) * 180.f / 3.14;
+	return atan2(yOff, xOff) * 180 / 3.14;
 }
+
+struct Coords
+{
+	double x, y;
+};
+
+struct Hero_
+{
+	Coords Position;
+	int CurrentPathIndex;
+	double DirectionFacing;
+	bool InPath;
+};
+
+global_variable Hero_ Hero;
+
+internal void
+RecordMouseClick(int x, int y)
+{
+	Clicks[ClicksSize].x = x;
+	Clicks[ClicksSize].y = y;
+	ClicksSize++;
+	if(Hero.InPath == false)
+	{
+		Hero.Position.x = Clicks[0].x;
+		Hero.Position.y = Clicks[0].y;
+		Hero.CurrentPathIndex = 0;
+	}
+	if(ClicksSize >= 2)
+	{
+		Hero.InPath = true;
+	}
+}
+
+internal void
+ClearMouseClicks()
+{
+	ClicksSize = 0;
+	Hero.InPath = false;
+}
+
+internal void
+NavigatePath(double Dt)
+{
+	if(Hero.InPath && Hero.CurrentPathIndex+1 != ClicksSize)
+	{
+		double Direction;
+
+		Direction = GetAngleBetweenPoints(Clicks[Hero.CurrentPathIndex],
+										Clicks[Hero.CurrentPathIndex+1]);
+
+		if(GetDistanceBetweenPoints(Hero.Position.x, Hero.Position.y,
+									Clicks[Hero.CurrentPathIndex + 1].x,
+									Clicks[Hero.CurrentPathIndex + 1].y)
+									< 50 * Dt)
+		{
+			Hero.CurrentPathIndex++;
+			Hero.Position.x = Clicks[Hero.CurrentPathIndex].x;
+			Hero.Position.y = Clicks[Hero.CurrentPathIndex].y;
+		}
+		else
+		{
+			Hero.Position.x += cos(Direction * 3.14 / 180.f) * Dt * 50;
+			Hero.Position.y += sin(Direction * 3.14 / 180.f) * Dt * 50;
+		}
+		Hero.DirectionFacing = Direction;
+	}
+}
+
+
 
 internal void
 DrawTriangle(int X, int Y, double Angle, int HalfHeight)
@@ -84,6 +151,12 @@ main(int argc, char* args[])
 	SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4,
 						SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
 	
+	Hero.Position.x = 0;
+	Hero.Position.y = 0;
+	Hero.DirectionFacing = 0;
+	Hero.CurrentPathIndex = 0;
+	Hero.InPath = false;
+
 	while(Running) {
 		dt = SDL_GetTicks() - lastTime;
 		lastTime = SDL_GetTicks();
@@ -115,6 +188,8 @@ main(int argc, char* args[])
 		snprintf(debugString, 100, "debug\n");
 		OutputDebugStringA(debugString);*/
 
+		NavigatePath(dt/1000.f);
+
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 		SDL_RenderClear(renderer);
 		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
@@ -131,8 +206,8 @@ main(int argc, char* args[])
 		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 		local_persist int Angle = 0;
 		Angle += .6 * dt;
-		DrawTriangle(Clicks[0].x, Clicks[0].y,
-					 GetAngleBetweenPoints(Clicks[0],Clicks[1]),
+		DrawTriangle(Hero.Position.x, Hero.Position.y,
+					 Hero.DirectionFacing,
 					 15);
 		SDL_RenderPresent(renderer);
 
